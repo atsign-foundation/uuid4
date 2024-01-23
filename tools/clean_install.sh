9975e238a39973a94ac5447ef395a927eeec28ce
Jeremy Tubongbanua <jeremy.tubongbanua@gmail.com>
2024-01-22 19:07:48 -0500

    feat: CMake installation

---
+++ tools/clean_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eu
+FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[0]}")"
+SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
+cd "$SCRIPT_DIRECTORY"
+cd ..
+rm -rf build
+cmake -S . -B build
+sudo cmake --build build --target install
